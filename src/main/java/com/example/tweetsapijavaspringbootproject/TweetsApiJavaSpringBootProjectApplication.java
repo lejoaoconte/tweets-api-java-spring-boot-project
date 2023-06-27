2a14e1e071ea2baca6cbdc1083a0168186fd3028
Your Name <lejoaoconte@gmail.com>
2023-06-26 23:56:26 -0300

    first commit

---
+++ src/main/java/com/example/tweetsapijavaspringbootproject/TweetsApiJavaSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.tweetsapijavaspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetsApiJavaSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetsApiJavaSpringBootProjectApplication.class, args);
+	}
+
+}
